de192143df3f053f36a9a7e980dbfca8d25dbc18
Tiit Hansen <hansen.tiit@gmail.com>
2024-11-23 10:47:32 +0200

    fix: Make it possible to send parallel bursts

--- tester/main.js
+++ tester/main.js
@@ -11,6 +11,10 @@ const responseDurationSeconds = new promClient.Histogram({
     buckets: [.005, .01, .025, .05, .075, .1, .125, .15, .175, .2, .25, .3, .4, .5, 1, 2.5, 5, 10]
 });
 
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
 function call(workDuration) {
     const end = responseDurationSeconds.startTimer()
     return fetch(`${TEST_TARGET}?duration=${workDuration}`, {
@@ -23,20 +27,28 @@ function call(workDuration) {
     .catch(error => console.log(error))
 }
 
-async function runInternal(duration, workDuration) {
+async function runInternal(duration, workDuration, parallelBurst, delay) {
+    
     const start = Date.now();
     while (Date.now() - start < duration) {
-        await call(workDuration);
+        const burst = [];
+        for (let i = 0; i < parallelBurst; i++) {
+            burst.push(call(workDuration));
+        }
+
+        await Promise.all(burst);
+        
+        await sleep(delay);
     }
 
     return true;
 }
 
-async function run(parallel, duration, workDuration) {
+async function run(parallel, duration, workDuration, parallelBurst, delay) {
 
     const all = [];
     for (let i = 0; i < parallel; i++) {
-        all.push(runInternal(duration, workDuration));
+        all.push(runInternal(duration, workDuration, parallelBurst, delay));
     }
 
     await Promise.all(all);
@@ -49,8 +61,11 @@ app.get("/run-test", (req, res) => {
     const parallel = parseInt(req.query.parallel);
     const duration = parseInt(req.query.duration);
     const workDuration = req.query.work_duration;
+    const delay = parseInt(req.query.delay);
+    const parallelBurst = parseInt(req.query.parallel_burst);
 
-    run(parallel, duration * MINUTES_TO_MILLISECONDS, workDuration).then(() => {
+    console.info(`Running test with parallel=${parallel}, duration=${duration}, workDuration=${workDuration}, parallelBurst=${parallelBurst}, delay=${delay}`);
+    run(parallel, duration * MINUTES_TO_MILLISECONDS, workDuration, parallelBurst, delay).then(() => {
         console.info(`Test completed`);
     });
     res.send("OK");
